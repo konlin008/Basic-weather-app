86b866c92262c863dbce80a628f2f50eabcb4f92
AMAN MONDAL <162113539+konlin008@users.noreply.github.com>
2024-09-12 20:09:42 +0530

    complete

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-// https://api.weatherapi.com/v1/current.json?key=1fdda0e159724497ad3184843240309&q=London&aqi=yes
 
 const apiKey = "1fdda0e159724497ad3184843240309";
 const apiurl = "https://api.weatherapi.com/v1/current.json?aqi=yes";
-const searchBar=document.querySelector(".search_bar")
-const searchBtn=document.querySelector(".search_btn")
+const searchBar = document.querySelector(".search_bar");
+const searchBtn = document.querySelector(".search_btn");
+const weatherIcon = document.querySelector(".weather_icon");
 
 async function checkWeather(city) {
-  const response = await fetch(apiurl +`&q=${city}` +`&key=${apiKey}`);
-  const data = await response.json();
-  document.querySelector("#temp").innerHTML = data.location.name;
-  document.querySelector("#cityName").innerHTML = Math.round(data.current.temp_c)+ "°C";
-  document.querySelector("#humid").innerHTML = data.current.humidity + "%";
-  document.querySelector("#humid").innerHTML = data.current.wind_kph + "Km/h";
+  const response = await fetch(apiurl + `&q=${city}` + `&key=${apiKey}`);
+  if (response.status == 400) {
+    document.querySelector("#errorMsg").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    const data = await response.json();
+    document.querySelector("#temp").innerHTML = data.location.name;
+    document.querySelector("#cityName").innerHTML =
+      Math.round(data.current.temp_c) + "°C";
+    document.querySelector("#humid").innerHTML = data.current.humidity + "%";
+    document.querySelector("#wind").innerHTML = data.current.wind_kph + "Km/h";
+    weatherIcon.src = data.current.condition.icon;
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector("#errorMsg").style.display = "none";
+    console.log(data);
+  }
 }
-searchBtn.addEventListener("click",()=>{
-    checkWeather(searchBar.value); 
-})
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBar.value);
+});
